485586f210e6bd33bf00112516efe774bee4f7c2
AndriesDB <andries.debaerz@gmail.com>
2023-04-12 15:44:21 +0200

    Fixing messages when deployed

--- resources/views/vendor/Chatify/layouts/messageCard.blade.php
+++ resources/views/vendor/Chatify/layouts/messageCard.blade.php
@@ -2,7 +2,7 @@
     @if($from_id != $to_id)
     <div class="message-card" data-id="{{ $id }}">
         <p>{!! ($message == null && $attachment != null && @$attachment[2] != 'file') ? $attachment[1] : nl2br($message) !!}
-            <sub title="{{ $fullTime }}">{{ $time }}</sub>
+            <sub ">{{ $time }}</sub>
             {{-- If attachment is a file --}}
             @if(@$attachment[2] == 'file')
             <a href="{{ route(config('chatify.attachments.download_route_name'), ['fileName'=>$attachment[0]]) }}" style="color: #595959;" class="file-download">
@@ -19,12 +19,12 @@
 
 {{-- -------------------- Sender card (owner) -------------------- --}}
 @if($viewType == 'sender')
-    <div class="message-card mc-sender" title="{{ $fullTime }}" data-id="{{ $id }}">
+    <div class="message-card mc-sender" data-id="{{ $id }}">
         <div class="chatify-d-flex chatify-align-items-center" style="flex-direction: row-reverse; justify-content: flex-end;">
             <i class="fas fa-trash chatify-hover-delete-btn" data-id="{{ $id }}"></i>
             <p style="margin-left: 5px;">
                 {!! ($message == null && $attachment != null && @$attachment[2] != 'file') ? $attachment[1] : nl2br($message) !!}
-                <sub title="{{ $fullTime }}" class="message-time">
+                <sub class="message-time">
                     <span class="fas fa-{{ $seen > 0 ? 'check-double' : 'check' }} seen"></span> {{ $time }}</sub>
                 </sub>
                 {{-- If attachment is a file --}}
