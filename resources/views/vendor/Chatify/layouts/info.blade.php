bc8a89c565335cbddc0356e5980c6c3e211be631
AndriesDB <andries.debaerz@gmail.com>
2023-04-12 16:28:24 +0200

    Fixing messages when deployed

--- resources/views/vendor/Chatify/layouts/info.blade.php
+++ resources/views/vendor/Chatify/layouts/info.blade.php
@@ -1,14 +1,11 @@
 {{-- user info and avatar --}}
-<div class="avatar av-l chatify-d-flex">
-    <img src="{{ asset('../../storage/'.$user->avatar) }}" alt="avatar">
-</div>
+<div class="avatar av-l chatify-d-flex"></div>
 <p class="info-name">{{ config('chatify.name') }}</p>
 <div class="messenger-infoView-btns">
-    {{-- <a href="#" class="default"><i class="fas fa-camera"></i> default</a> --}}
-    <a href="#" class="danger delete-conversation"><i class="fas fa-trash-alt"></i> Verwijder conversatie</a>
+    <a href="#" class="danger delete-conversation">Delete Conversation</a>
 </div>
 {{-- shared photos --}}
 <div class="messenger-infoView-shared">
-    <p class="messenger-title">Gedeelde foto's</p>
+    <p class="messenger-title"><span>Shared Photos</span></p>
     <div class="shared-photos-list"></div>
 </div>
--- resources/views/vendor/Chatify/layouts/listItem.blade.php
+++ resources/views/vendor/Chatify/layouts/listItem.blade.php
@@ -1,24 +1,28 @@
 {{-- -------------------- Saved Messages -------------------- --}}
 @if($get == 'saved')
-    <table class="messenger-list-item m-li-divider" data-contact="{{ Auth::user()->id }}">
+    <table class="messenger-list-item" data-contact="{{ Auth::user()->id }}">
         <tr data-action="0">
             {{-- Avatar side --}}
             <td>
-            <div class="avatar av-m" style="color: black; background-color: #d9efff; text-align: center; display: flex; flex-direction: column; align-items: center; justify-content: center;">
-                <span class="far fa-bookmark" style="font-size: 22px; color: #68a5ff;"></span>
+            <div class="saved-messages avatar av-m">
+                <span class="far fa-bookmark"></span>
             </div>
             </td>
             {{-- center side --}}
             <td>
-                <p data-id="{{ Auth::user()->id }}" data-type="user">Opgeslagen berichten <span>Jij</span></p>
-                <span>Geheime opgeslagen berichten</span>
+                <p data-id="{{ Auth::user()->id }}" data-type="user">Saved Messages <span>You</span></p>
+                <span>Save messages secretly</span>
             </td>
         </tr>
     </table>
 @endif
 
-{{-- -------------------- All users/group list -------------------- --}}
-@if($get == 'users')
+{{-- -------------------- Contact list -------------------- --}}
+@if($get == 'users' && !!$lastMessage)
+<?php
+$lastMessageBody = mb_convert_encoding($lastMessage->body, 'UTF-8', 'UTF-8');
+$lastMessageBody = strlen($lastMessageBody) > 30 ? mb_substr($lastMessageBody, 0, 30, 'UTF-8').'..' : $lastMessageBody;
+?>
 <table class="messenger-list-item" data-contact="{{ $user->id }}">
     <tr data-action="0">
         {{-- Avatar side --}}
@@ -26,12 +30,15 @@
             @if($user->active_status)
                 <span class="activeStatus"></span>
             @endif
+        <div class="avatar av-m"
+        style="background-image: url('{{ $user->avatar }}');">
+        </div>
         </td>
         {{-- center side --}}
         <td>
         <p data-id="{{ $user->id }}" data-type="user">
-            {{ strlen($user->name) > 30 ? trim(substr($user->name,0,30)).'..' : $user->name }}
-            <span>{{ $lastMessage->created_at->diffForHumans() }}</span></p>
+            {{ strlen($user->name) > 12 ? trim(substr($user->name,0,12)).'..' : $user->name }}
+            <span class="contact-item-time" data-time="{{$lastMessage->created_at}}">{{ $lastMessage->timeAgo }}</span></p>
         <span>
             {{-- Last Message user indicator --}}
             {!!
@@ -42,18 +49,15 @@
             {{-- Last message body --}}
             @if($lastMessage->attachment == null)
             {!!
-                strlen($lastMessage->body) > 30
-                ? trim(substr($lastMessage->body, 0, 30)).'..'
-                : $lastMessage->body
+                $lastMessageBody
             !!}
             @else
-            <span class="fas fa-file"></span> Bijlage
+            <span class="fas fa-file"></span> Attachment
             @endif
         </span>
         {{-- New messages counter --}}
             {!! $unseenCounter > 0 ? "<b>".$unseenCounter."</b>" : '' !!}
         </td>
-
     </tr>
 </table>
 @endif
@@ -65,13 +69,13 @@
         {{-- Avatar side --}}
         <td>
         <div class="avatar av-m"
-        style="background-image: url('../../storage/{{ $user->avatar }}');">
+        style="background-image: url('{{ $user->avatar }}');">
         </div>
         </td>
         {{-- center side --}}
         <td>
             <p data-id="{{ $user->id }}" data-type="user">
-            {{ strlen($user->name) > 30 ? trim(substr($user->name,0,30)).'..' : $user->name }}
+            {{ strlen($user->name) > 12 ? trim(substr($user->name,0,12)).'..' : $user->name }}
         </td>
 
     </tr>
