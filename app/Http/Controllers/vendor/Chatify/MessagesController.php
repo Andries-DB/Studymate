9925e24eae62f3bc3ede712da57bf98c15a067fc
AndriesDB <andries.debaerz@gmail.com>
2023-04-12 12:33:36 +0200

    Fixing messages when deployed

--- app/Http/Controllers/vendor/Chatify/MessagesController.php
+++ app/Http/Controllers/vendor/Chatify/MessagesController.php
@@ -148,6 +148,7 @@ class MessagesController extends Controller
         if (!$error->status) {
             // send to database
             $messageID = mt_rand(9, 999999999) + time();
+            dd($messageID);
             Chatify::newMessage([
                 'id' => $messageID,
                 'type' => $request['type'],
