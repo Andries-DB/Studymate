b23b1e6521ab66bc4d4bee29c670be9ee802ba38
AndriesDB <andries.debaerz@gmail.com>
2023-04-12 15:57:20 +0200

    Fixing messages when deployed

--- app/Models/ChMessage.php
+++ app/Models/ChMessage.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class ChMessage extends Model
 {
-    protected $fillable = ['id', 'from_id', 'to_id', 'message', 'is_seen', 'is_deleted', 'is_sent', 'is_delivered', 'is_read', 'created_at', 'updated_at'];
+    protected $fillable = ['chatify_id', 'from_id', 'to_id', 'message', 'is_seen', 'is_deleted', 'is_sent', 'is_delivered', 'is_read', 'created_at', 'updated_at'];
 }
